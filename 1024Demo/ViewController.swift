418d8a0470cb870e8eb927b9b92240906a6a9235
Vishnu Nair <vishnubn92@gmail.com>
2021-08-23 07:36:21 +0530

    Initial Commit

---
+++ 1024Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1024Demo
+//
+//  Created by Vishnu  Nair on 23/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
